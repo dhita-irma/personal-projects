2bd8d26046e176c61757d7f67de234c73b3fc115
Dhita Irma <dhita.irma@gmail.com>
2020-08-06 11:34:21 +0800

    Render entry pages

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
+markdowner = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "content": markdowner.convert(str(util.get_entry(title))),
+        "title": title
+    })
